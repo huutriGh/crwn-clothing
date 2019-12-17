75570badb33fa1d7641091acd10a44b9d41ae685
unknown <huutriqt13@hotmail.com>
2019-12-17 15:06:44 +0700

    finishing sign in handlesubmit method

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ class App extends React.Component {
               ...snapshot.data()
             }
           });
-          console.log(this.state);
         });
       }
       this.setState({ currentUser: userAuth });
