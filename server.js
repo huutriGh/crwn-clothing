a76599f25414b661d44239c1eb3f4da72d623fae
unknown <huutriqt13@hotmail.com>
2020-01-02 15:48:19 +0700

    updateing our code to be pwa compliant, added serviceworker, https, updated manifest

--- server.js
+++ server.js
@@ -13,10 +13,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(compression);
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
