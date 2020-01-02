fdfcada67e9142e92417fe84d62b6d245808f9ed
unknown <huutriqt13@hotmail.com>
2020-01-02 15:42:06 +0700

    updateing our code to be pwa compliant, added serviceworker, https, updated manifest

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -12,13 +13,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(compression);
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
